354881692ab18f1046e4e7f059ff71970a933d7e
Chapmamba <918577248@qq.com>
2022-10-08 21:17:32 +0800

    first commit

---
+++ src/main/java/com/chap/git/GitMain.java
@@ -0,0 +1,11 @@
+package com.chap.git;
+
+/**
+ * @Author:chapman
+ * @Date: Created in 21:14 2022/10/8
+ */
+public class GitMain {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/com/chap/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.chap.git;
+
+/**
+ * @Author:chapman
+ * @Date: Created in 21:12 2022/10/8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
